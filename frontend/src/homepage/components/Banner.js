79f7952d1610da4faa382415cfa970f568a816bf
dep50 <dep500@gmail.com>
2023-06-04 22:10:11 +0700

    Editted Landing Page

--- frontend/src/homepage/components/Banner.js
+++ frontend/src/homepage/components/Banner.js
@@ -56,9 +56,7 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to OptiClinicus</span>
-                <h1>{`Your Accessible Healthcare for all`} </h1>
-                  <p>OptiClinicus is a cutting-edge telemedicine application that brings accessible and convenient healthcare right to your fingertips. With OptiClinicus, you can easily connect with experienced doctors and healthcare professionals for virtual consultations, receive expert medical advice, and get prescribed medication delivered right to your doorstep. Our user-friendly interface and secure platform ensure a seamless and confidential experience. Whether you're seeking routine check-ups, urgent care, or medication refills, OptiClinicus is here to provide personalized healthcare solutions tailored to your needs. Experience the future of healthcare with OptiClinicus and take control of your well-being anytime, anywhere.</p>
-                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+                <h1>{`Your Accessible Healthcare for all`} </h1>  
               </div>}
             </TrackVisibility>
           </Col>
--- frontend/src/homepage/components/Projects.js
+++ frontend/src/homepage/components/Projects.js
@@ -50,43 +50,8 @@ export const Projects = () => {
             <TrackVisibility>
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
+                <h2>About Us</h2>
+                <p>OptiClinicus is a cutting-edge telemedicine application that brings accessible and convenient healthcare right to your fingertips. With OptiClinicus, you can easily connect with experienced doctors and healthcare professionals for virtual consultations, receive expert medical advice, and get prescribed medication delivered right to your doorstep. Our user-friendly interface and secure platform ensure a seamless and confidential experience. Whether you're seeking routine check-ups, urgent care, or medication refills, OptiClinicus is here to provide personalized healthcare solutions tailored to your needs. Experience the future of healthcare with OptiClinicus and take control of your well-being anytime, anywhere.</p>
               </div>}
             </TrackVisibility>
           </Col>
